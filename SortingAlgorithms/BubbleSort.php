36c7f56cb14dfa2a647f719b1a0e73960b75e75d
Noman Kabeer <nomankabeerr@gmail.com>
2019-10-11 02:08:16 +0500

    bubble sort

--- SortingAlgorithms/BubbleSort.php
+++ SortingAlgorithms/BubbleSort.php
@@ -1,24 +1,22 @@
 <?php
-// Write an function to sort an array
-function BubbleSort($res){
-     $size = count($res);
-                for($i=0;$i<$size;$i++)
-                {
-                    for($j=0;$j<$size;$j++)
-                    {
-                        if($res[$i] < $res[$j])
-                       {
-                            $temp = $res[$i];
-                           $res[$i] = $res[$j];
-                           $res[$j] = $temp;
-                       }
-                   }
-                }
-                return $res;
+//Bubble sort function to sort an array
+function BubbleSort($res)
+{
+    $size = count($res);
+    for ($i = 0; $i < $size; $i++) {
+        for ($j = 0; $j < $size; $j++) {
+            if ($res[$i] < $res[$j]) {
+                $temp = $res[$i];
+                $res[$i] = $res[$j];
+                $res[$j] = $temp;
+            }
+        }
+    }
+    return $res;
 }
-$res = array(2,3,4,5,6,6,7,6,5,4,4,65);
+$res = array(2, 3, 4, 5, 6, 6, 7, 6, 5, 4, 4, 65);
 $sorted_array = BubbleSort($res);
 foreach ($sorted_array as $key => $value) {
-  echo $value.'<br>';
+    echo $value . '<br>';
 }
 ?>
